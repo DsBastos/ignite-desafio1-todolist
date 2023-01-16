207b1a8d986284a911d7c4029a420a94a26e3c75
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2023-01-16 17:45:43 +0000

    ui da lista de tasks complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { Header } from './components/Header'
+import { Tasks } from './components/Tasks'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Tasks />
     </>
   )
 }
---
+++ src/components/Tasks/index.tsx
@@ -0,0 +1,24 @@
+import styles from './tasks.module.css';
+import { Task } from '../Task/index';
+
+export function Tasks() {
+  return (
+    <section className={styles.tasks}>
+      <header className={styles.header}>
+        <div>
+          <p>Tarefas criadas</p>
+          <span>10</span>
+        </div>
+        <div>
+          <p className={styles.textPurple}>Concluídas</p>
+          <span>2 de 10</span>
+        </div>
+      </header>
+
+      <div className={styles.list}>
+        <Task />
+        <Task />
+      </div>
+    </section>
+  )
+}
